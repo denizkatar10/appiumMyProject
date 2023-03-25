67a41079025aebe1074525afee3d946afb80a397
Deniz Katar <dnzkatar@gmail.com>
2023-03-25 02:29:02 -0400

    new added

---
+++ src/test/java/test/AppiumInspector02.java
@@ -0,0 +1,18 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class AppiumInspector02 {
+
+    @Test
+    public void test(){
+//    /*
+//    lunch GestureTool app
+//    click addGesture button
+//    send text to name box "testing"
+//    click done
+//    verify testing gesture is created
+//     */
+
+    }
+}
